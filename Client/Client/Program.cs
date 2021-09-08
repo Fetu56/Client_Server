36c6e075507e161b5ade9a01b54b1d44b7cf51ec
student <student@05-10>
2021-09-08 11:42:04 +0300

    Simple client server math operations without check data

--- Client/Client/Program.cs
+++ Client/Client/Program.cs
@@ -1,9 +1,39 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Client {
     class Program {
+        static string ip = "127.0.0.1";
+        static int port = 8000;
+
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            try {
+                socket.Connect(iPEndPoint);
+                int bytes = 0;
+                byte[] data = new byte[256];
+
+                data = Encoding.Unicode.GetBytes(Console.ReadLine());
+                socket.Send(data);
+                    StringBuilder stringBuilder = new StringBuilder();
+                while (true) {
+
+                    
+
+                    do {
+                        bytes = socket.Receive(data);
+                        stringBuilder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (socket.Available > 0);
+                    Console.WriteLine($"Got answer from server: {stringBuilder.ToString()}");
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
--- Client/Server/Program.cs
+++ Client/Server/Program.cs
@@ -1,9 +1,58 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace Server {
     class Program {
+        static int port = 8000;
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);
+            Socket socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            Console.WriteLine("Start server...");
+            try {
+                socket.Bind(iPEndPoint);
+                socket.Listen(10);
+
+                while (true) {
+                    Socket socketClient = socket.Accept();
+
+                    StringBuilder stringBuilder = new StringBuilder();
+
+                    int bytes = 0;
+                    byte[] data = new byte[256];
+
+                    do {
+                        bytes = socketClient.Receive(data);
+                        stringBuilder.Append(Encoding.Unicode.GetString(data, 0, bytes));
+                    } while (socketClient.Available > 0);
+
+                    Console.WriteLine($"Got data {Encoding.Unicode.GetString(data)} form client, answer sended.");
+                    switch(Encoding.Unicode.GetString(data).Split(',')[2].ToCharArray()[0]) {
+                        case '+':
+                            data = Encoding.Unicode.GetBytes(Encoding.Unicode.GetString(data).Split(',')[0] + Encoding.Unicode.GetString(data).Split(',')[2] + Encoding.Unicode.GetString(data).Split(',')[1] + " = " + (int.Parse(Encoding.Unicode.GetString(data).Split(',')[0]) + int.Parse(Encoding.Unicode.GetString(data).Split(',')[1])).ToString());
+                            break;
+                        case '-':
+                            data = Encoding.Unicode.GetBytes(Encoding.Unicode.GetString(data).Split(',')[0] + Encoding.Unicode.GetString(data).Split(',')[2] + Encoding.Unicode.GetString(data).Split(',')[1] + " = " + (int.Parse(Encoding.Unicode.GetString(data).Split(',')[0]) - int.Parse(Encoding.Unicode.GetString(data).Split(',')[1])).ToString());
+                            break;
+                        case '*':
+                            data = Encoding.Unicode.GetBytes(Encoding.Unicode.GetString(data).Split(',')[0] + Encoding.Unicode.GetString(data).Split(',')[2] + Encoding.Unicode.GetString(data).Split(',')[1] + " = " + (int.Parse(Encoding.Unicode.GetString(data).Split(',')[0]) * int.Parse(Encoding.Unicode.GetString(data).Split(',')[1])).ToString());
+                            break;
+                        case '/':
+                            data = Encoding.Unicode.GetBytes(Encoding.Unicode.GetString(data).Split(',')[0] + Encoding.Unicode.GetString(data).Split(',')[2] + Encoding.Unicode.GetString(data).Split(',')[1] + " = " + (int.Parse(Encoding.Unicode.GetString(data).Split(',')[0]) / int.Parse(Encoding.Unicode.GetString(data).Split(',')[1])).ToString());
+                            break;
+                    }
+                    
+                    socketClient.Send(data);
+
+                    //socketClient.Shutdown(SocketShutdown.Both);
+                    //socketClient.Close();
+                }
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
